381296e0fa40607839a2ab899ad9d619beafcc83
UncleSam <shrimp509@hotmail.com>
2022-12-01 17:51:11 +0800

    Add style of home page

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module AutoPlacer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.precompile << %w( TaipeiSansTCBeta-Regular.ttf )
   end
 end
