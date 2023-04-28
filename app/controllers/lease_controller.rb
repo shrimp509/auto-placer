15dd6d2628ef22ac4c75b53be496655c47ba09f5
UncleSam <shrimp509@hotmail.com>
2023-04-28 09:09:59 +0800

    Finish lease page v1

--- app/controllers/lease_controller.rb
+++ app/controllers/lease_controller.rb
@@ -3,8 +3,112 @@ class LeaseController < ApplicationController
   end
 
   def start
+    lease_page_1 = "#{Rails.root}/tmp/#{params[:leaseholder]}-租約-第一頁.png"
+    lease_page_2 = "#{Rails.root}/tmp/#{params[:leaseholder]}-租約-第二頁.png"
+    lease_page_3 = "#{Rails.root}/tmp/#{params[:leaseholder]}-租約-第三頁.png"
+
+    `ffmpeg -y \
+      -i #{Rails.root}/app/assets/images/lease-1.jpg \
+      -vf "#{draw_text(params[:year], 830, 190, size: 30)},\
+          #{draw_text(params[:month], 1000, 190, size: 30)},\
+          #{draw_text(params[:date], 1150, 190, size: 30)},\
+          #{draw_text(params[:building_number], 425, 585, size: 30)},\
+          #{draw_text(params[:house_number], 425, 645, size: 30)},\
+          #{draw_text(params[:car_capacity], 455, 705, size: 30)},\
+          #{draw_text(params[:bike_capacity], 455, 765, size: 30)},\
+          #{draw_text(params[:main_building], 455, 825, size: 30)},\
+          #{draw_text(params[:affiliated_building], 495, 895, size: 30)},\
+          #{draw_text(params[:shared_building_number], 560, 960, size: 30)},\
+          #{draw_text(params[:parking_car_floor], 605, 1270, size: 30)},\
+          #{draw_text(params[:parking_car_number], 450, 1310, size: 30)},\
+          #{draw_text('V', x_option_parking_type, 1270, size: 30)},\
+          #{draw_text(params[:parking_bike_floor], 805, 1345, size: 30)},\
+          #{draw_text(params[:parking_bike_number], 1020, 1345, size: 30)},\
+          #{draw_text('V', x_option_parking_period, 1465, size: 30)},\
+          #{draw_text("`#{params[:other]}`", 807, 1460, size: 30)},\
+          #{draw_text(params[:lease_period_from_year], 615, 1625, size: 30)},\
+          #{draw_text(params[:lease_period_from_month], 780, 1625, size: 30)},\
+          #{draw_text(params[:lease_period_from_day], 930, 1625, size: 30)},\
+          #{draw_text(params[:lease_period_to_year], 390, 1665, size: 30)},\
+          #{draw_text(params[:lease_period_to_month], 550, 1665, size: 30)},\
+          #{draw_text(params[:lease_period_to_day], 715, 1665, size: 30)},\
+          #{draw_text(params[:lease_price], 870, 1750, size: 30)},\
+          #{draw_text(params[:option_lease_unit_month_day], 820, 1785, size: 30)}" #{lease_page_1}`
+
+    `ffmpeg -y \
+      -i #{Rails.root}/app/assets/images/lease-2.jpg \
+      -vf "#{draw_text(params[:bank_name], 1110, 185, size: 30)},\
+            #{draw_text(params[:bank_user_name], 490, 225, size: 30)},\
+            #{draw_text(params[:bank_user_account], 790, 230, size: 24)}" #{lease_page_2}`
+
+    `ffmpeg -y \
+      -i #{Rails.root}/app/assets/images/lease-3.jpg \
+      -vf "#{draw_text(params[:leaseholder], 420, 239, size: 30)},\
+            #{draw_text(params[:leaseholder_id_number], 680, 290, size: 30)},\
+            #{draw_text(params[:leaseholder_address], 610, 335, size: 30)},\
+            #{draw_text(params[:leaseholder_comm_address], 385, 385, size: 30)},\
+            #{draw_text(params[:leaseholder_phone], 385, 430, size: 30)},\
+            #{draw_text(params[:leasee], 420, 530, size: 30)},\
+            #{draw_text(params[:leasee_id_number], 680, 580, size: 30)},\
+            #{draw_text(params[:leasee_address], 610, 625, size: 30)},\
+            #{draw_text(params[:leasee_comm_address], 385, 675, size: 30)},\
+            #{draw_text(params[:leasee_phone], 385, 725, size: 30)}" #{lease_page_3}`
+
+    require 'zip'
+    zip_name = "#{params[:building_number]}.zip"
+    Zip::File.open("tmp/#{zip_name}", Zip::File::CREATE) do |zip|
+      zip.add(File.basename(lease_page_1), lease_page_1)
+      zip.add(File.basename(lease_page_2), lease_page_2)
+      zip.add(File.basename(lease_page_3), lease_page_3)
+    end
+
+    @name = zip_name
+  ensure
+    # [lease_page_1, lease_page_2, lease_page_3].each { |file| File.delete(file) }
   end
 
   def download
+    name = CGI.unescape(params[:name])
+    file = "tmp/#{name}"
+    send_file(file) if File.exist?(file)
+  ensure
+    other_zips = Dir.glob("tmp/*.zip") - [file]
+    other_zips.each { |file| File.delete(file) }
+  end
+
+  private
+
+  def font_file
+    "fontfile=#{Rails.root}/app/assets/fonts/TaipeiSansTCBeta-Regular.ttf"
+  end
+
+  def draw_text(text, x, y, color: 'black', size: 22)
+    "drawtext=#{font_file}:text='#{text}':fontcolor=#{color}:fontsize=#{size}:x=#{x}:y=#{y}"
+  end
+
+  def x_option_parking_period
+    case params[:option_parking_period]
+    when '全日'
+      407
+    when '日間'
+      507
+    when '夜間'
+      607
+    when '其他'
+      707
+    else
+      707
+    end
+  end
+
+  def x_option_parking_type
+    case params[:option_parking_type]
+    when '平面車位'
+      700
+    when '機械車位'
+      930
+    else
+      700
+    end
   end
 end
